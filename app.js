efce5eccbd532320ab91e3297ae3c3348db70e43
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-15 11:49:04 +0800

    lesson 64

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 var uiController = (function () {
-
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn: ".add__btn"
+    };
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
 })();
 
 var financeController = (function () { })();
 
 var appController = (function (uiController, financeController) {
+    var DOM = uiController.getDOMstrings();
+
     var ctrlAddItem = function () {
         console.log('Delgetsnees ugugdul avah heseg');
+        console.log(uiController.getInput());
     }
-    document.querySelector('.add__btn').addEventListener('click', function () {
+    document.querySelector(DOM.addBtn).addEventListener('click', function () {
         ctrlAddItem();
     });
     document.addEventListener("keypress", function (event) {
