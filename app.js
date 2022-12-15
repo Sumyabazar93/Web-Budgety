6d6c5c66db6bdd6973e59a1126d5f097eb9abd7e
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-15 15:14:54 +0800

    html.replace, insertAdjacentHTML1

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var uiController = (function () {
     var DOMstrings = {
         inputType: ".add__type",
-        inputDescription: '.add__description',
-        inputValue: '.add__value',
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
         addBtn: ".add__btn"
     };
+
     return {
         getInput: function () {
             return {
@@ -13,30 +14,29 @@ var uiController = (function () {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+
         getDOMstrings: function () {
             return DOMstrings;
         },
 
         addListItem: function (item, type) {
             var html, list;
-
             if (type === "inc") {
                 list = ".income__list";
-
                 html =
-                    '<div class="item clearfix" id="income-%id%">< div class="item__description" >$$DESCRIPTION$$ </ >< div class="right clearfix" ><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></ ></div >';
+                    '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
             } else {
-                list = '.expenses__title';
+                list = ".expenses__list";
                 html =
-                    '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                    '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
-            html = html.replace('%id%', item.id);
-            html = html.replace('$$DESCRIPTION$$', item.description);
-            html = html.replace('$$VALUE$$', item.value);
+            html = html.replace("%id%", item.id);
+            html = html.replace("$$DESCRIPTION$$", item.description);
+            html = html.replace("$$VALUE$$", item.value);
 
             document.querySelector(list).insertAdjacentHTML("beforeend", html);
         }
-    }
+    };
 })();
 
 var financeController = (function () {
@@ -44,48 +44,51 @@ var financeController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
 
     var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
-    var i1 = new Income(1, 'Money', 12345678);
+    };
 
     var data = {
         items: {
             inc: [],
             exp: []
         },
+
         totals: {
             inc: 0,
             exp: 0
         }
-    }
+    };
+
     return {
         addItem: function (type, desc, val) {
-            console.log('item added...');
-
             var item, id;
 
             if (data.items[type].length === 0) id = 1;
             else {
                 id = data.items[type][data.items[type].length - 1].id + 1;
             }
+
             if (type === "inc") {
                 item = new Income(id, desc, val);
             } else {
                 item = new Expense(id, desc, val);
             }
+
             data.items[type].push(item);
+
+            return item;
         },
+
         seeData: function () {
             return data;
         }
     };
 })();
-
 var appController = (function (uiController, financeController) {
     var ctrlAddItem = function () {
         var input = uiController.getInput();
@@ -95,29 +98,31 @@ var appController = (function (uiController, financeController) {
             input.description,
             input.value
         );
+
         uiController.addListItem(item, input.type);
-        // console.log(input);
-        // financeController.addItem(input.type, input.description, input.value);
-    }
+
+    };
+
     var setupEventListeners = function () {
         var DOM = uiController.getDOMstrings();
 
-        document.querySelector(DOM.addBtn).addEventListener('click', function () {
+        document.querySelector(DOM.addBtn).addEventListener("click", function () {
             ctrlAddItem();
         });
+
         document.addEventListener("keypress", function (event) {
-            if (event.keyCode === 13) {
+            if (event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
             }
         });
-    }
+    };
+
     return {
         init: function () {
-            console.log("Aplication started...");
+            console.log("Application started...");
             setupEventListeners();
         }
-    }
-
+    };
 })(uiController, financeController);
 
 appController.init();
