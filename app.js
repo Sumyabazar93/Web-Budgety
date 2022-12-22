612979e208dde6413511d8956147ed33cdfb1899
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-22 11:02:19 +0800

    Tusuv haruulah function done

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var uiController = (function () {
         inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        addBtn: ".add__btn"
+        addBtn: ".add__btn",
+        incomeList: ".income__list",
+        expenseList: ".expenses__list",
+        tusuvLabel: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expenseLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage"
     };
 
     return {
@@ -11,7 +17,7 @@ var uiController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseInt(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -19,14 +25,43 @@ var uiController = (function () {
             return DOMstrings;
         },
 
+        clearFields: function () {
+            var fields = document.querySelectorAll(
+                DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+            );
+
+            // Convert List to Array
+
+            var fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (el, index, array) {
+                el.value = "";
+            });
+
+            fieldsArr[0].focus();
+            // for(var i = 0; i < fieldsArr.length; i ++) {
+            //     fieldsArr[i].value = "";
+            // }
+        },
+        tusviigUzuuleh: function (tusuv) {
+            document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+            document.querySelector(DOMstrings.incomeLabel).textContent = tusuv.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = tusuv.totalExp;
+
+            if (tusuv.huvi === 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi;
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi + "%";
+            }
+        },
         addListItem: function (item, type) {
             var html, list;
             if (type === "inc") {
-                list = ".income__list";
+                list = DOMstrings.incomeList;
                 html =
                     '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
             } else {
-                list = ".expenses__list";
+                list = DOMstrings.expenseList;
                 html =
                     '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
@@ -51,7 +86,13 @@ var financeController = (function () {
         this.description = description;
         this.value = value;
     };
-
+    var calculateTotal = function (type) {
+        var sum = 0;
+        data.items[type].forEach(function (el) {
+            sum += el.value;
+        });
+        data.totals[type] = sum;
+    }
     var data = {
         items: {
             inc: [],
@@ -61,10 +102,31 @@ var financeController = (function () {
         totals: {
             inc: 0,
             exp: 0
-        }
+        },
+
+        tusuv: 0,
+
+        huvi: 0
     };
 
     return {
+        tusuvTootsooloh: function () {
+            calculateTotal('inc');
+            calculateTotal('exp');
+
+            data.tusuv = data.totals.inc - data.totals.exp;
+
+            data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+        },
+        tusviigAvah: function () {
+            return {
+                tusuv: data.tusuv,
+                huvi: data.huvi,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp
+            }
+        },
+
         addItem: function (type, desc, val) {
             var item, id;
 
@@ -93,14 +155,22 @@ var appController = (function (uiController, financeController) {
     var ctrlAddItem = function () {
         var input = uiController.getInput();
 
-        var item = financeController.addItem(
-            input.type,
-            input.description,
-            input.value
-        );
+        if (input.description !== "" && input.value !== "") {
+            var item = financeController.addItem(
+                input.type,
+                input.description,
+                input.value
+            );
+            uiController.addListItem(item, input.type);
+            uiController.clearFields();
 
-        uiController.addListItem(item, input.type);
+            financeController.tusuvTootsooloh();
 
+            var tusuv = financeController.tusviigAvah();
+
+            uiController.tusviigUzuuleh(tusuv);
+            console.log(tusuv);
+        }
     };
 
     var setupEventListeners = function () {
@@ -120,6 +190,12 @@ var appController = (function (uiController, financeController) {
     return {
         init: function () {
             console.log("Application started...");
+            uiController.tusviigUzuuleh({
+                tusuv: 0,
+                huvi: 0,
+                totalInc: 0,
+                totalExp: 0
+            });
             setupEventListeners();
         }
     };
